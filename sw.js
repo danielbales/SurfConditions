31a47c4a1d45b27ef8a4611cdc0ebd7e7b4963f3
Daniel Bales <daniel@sharpeyeanimation.com>
2026-09-17 22:54:39 -0700

    Deploy: labeled badges

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'surf-conditions-v30';
+const CACHE_NAME = 'surf-conditions-v31';
 const WORKER_URL = 'https://surf-alerts.dbales1210.workers.dev';
 const APP_SHELL = [
   '/SurfConditions/',
