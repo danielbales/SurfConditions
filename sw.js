2eaa27df14f8d95985628d10a5cf347ad9aec180
Daniel Bales <daniel@sharpeyeanimation.com>
2026-09-23 21:24:19 -0700

    Deploy: smart label placement on buoy map

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'surf-conditions-v42';
+const CACHE_NAME = 'surf-conditions-v43';
 const WORKER_URL = 'https://surf-alerts.dbales1210.workers.dev';
 const APP_SHELL = [
   '/SurfConditions/',
